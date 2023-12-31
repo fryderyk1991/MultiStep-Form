bb14cb684211c5cb39df5236f0cff2571a750171
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-12-31 10:39:40 +0100

    added Value to Dropdown item

--- src/components/DropDown/DropDown.js
+++ src/components/DropDown/DropDown.js
@@ -9,17 +9,22 @@ import { IoMdArrowDropdown } from "react-icons/io";
 
 const DropDown = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [value, setValue] = useState('Drop');
+
+  const handleValue = (e) => {
+    setValue(e.target.textContent)
+  }
 
   return (
     <DropdownContainer>
-        <div>Drop</div>
+        <div>{value}</div>
       <DropDownList $isOpen={isOpen}>
-       <DropDownListItem $isOpen={isOpen}>Item 1</DropDownListItem>
-       <DropDownListItem $isOpen={isOpen}>Item 2</DropDownListItem>
-       <DropDownListItem $isOpen={isOpen}>Item 2</DropDownListItem>
-       <DropDownListItem $isOpen={isOpen}>Item 2</DropDownListItem>
+       <DropDownListItem onClick={(e) => {setIsOpen(!isOpen); handleValue(e)}} $isOpen={isOpen}>Item 1</DropDownListItem>
+       <DropDownListItem onClick={() => {setIsOpen(!isOpen)}} $isOpen={isOpen}>Item 2</DropDownListItem>
+       <DropDownListItem onClick={() => {setIsOpen(!isOpen)}} $isOpen={isOpen}>Item 2</DropDownListItem>
+       <DropDownListItem onClick={() => {setIsOpen(!isOpen)}} $isOpen={isOpen}>Item 2</DropDownListItem>
       </DropDownList>
-      <DropDownBtn onClick={() => {setIsOpen(!isOpen);console.log(isOpen)}} $isOpen={isOpen}><IoMdArrowDropdown /></DropDownBtn>
+      <DropDownBtn onClick={() => {setIsOpen(!isOpen)}} $isOpen={isOpen}><IoMdArrowDropdown /></DropDownBtn>
     </DropdownContainer>
   );
 };
